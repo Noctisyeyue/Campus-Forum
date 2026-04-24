755ba54c52f1b8c56c243ea98fe73de03e1972ad
yeyue <1923780217@qq.com>
2026-04-24 14:15:53 +0800

    完成--前端原项目迁移

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,24 @@
 <template>
-  <router-view />
+  <header>
+    <div class="wrapper">
+      <router-view/>
+    </div>
+  </header>
 </template>
+
+<script setup>
+import { useDark } from '@vueuse/core'
+
+useDark({
+  selector: 'html',
+  attribute: 'class',
+  valueDark: 'dark',
+  valueLight: 'light'
+})
+</script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import { createPinia } from 'pinia'
 import router from './router'
+import axios from 'axios'
+import { createPinia } from 'pinia'
+
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import '@/assets/quill.css'
+
+// 后端接口地址（新项目端口 8081）
+axios.defaults.baseURL = 'http://localhost:8081'
 
 const app = createApp(App)
 
