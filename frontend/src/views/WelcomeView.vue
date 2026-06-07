59dcac4b711d9e5b32a847c857b24661ef5cf77d
yeyue <1923780217@qq.com>
2026-06-07 18:30:04 +0800

    修改--修改首页图片

--- frontend/src/views/WelcomeView.vue
+++ frontend/src/views/WelcomeView.vue
@@ -2,10 +2,10 @@
     <div style="width: 100vw;height: 100vh;overflow: hidden;display: flex">
         <div style="flex: 1">
             <el-image style="width: 100%;height: 100%" fit="cover"
-                      src="https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800"/>
+                      src="https://zsb.tust.edu.cn/images/2022-12/20180119134706157509.jpg"/>
         </div>
         <div class="welcome-title">
-            <div style="font-size: 30px;font-weight: bold">欢迎来到 Campus Forum 校园论坛系统</div>
+            <div style="font-size: 30px;font-weight: bold">欢迎来到 Campus Forum 校园论坛</div>
             <div style="margin-top: 10px">在这里你可以与同学们交流学习心得，分享校园生活，参与话题讨论。</div>
             <div style="margin-top: 5px">发表帖子、评论互动、结识志同道合的朋友。</div>
         </div>
@@ -38,3 +38,4 @@
     text-shadow: 0 0 10px black;
 }
 </style>
+<!-- https://img1.baidu.com/it/u=4097856652,4033702227&fm=253&fmt=auto&app=120&f=JPEG?w=1422&h=800 -->
